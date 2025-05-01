b8a27439f441d342ce9ed692e3828f2b12ffe983
PrithulaSikder <prithulasikder2019@gmail.com>
2025-05-01 17:18:12 +0600

    rty

--- database.php
+++ database.php
@@ -9,7 +9,9 @@ $conn = new mysqli($servername, $name, $password, 'OAA_STUDENT_MANAGEMENT_SYSTEM
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-// echo "Connected successfully";
+else{
+    echo "ConnectedSuccessfully";
+}
 
 ?>
 
